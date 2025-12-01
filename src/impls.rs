14a447846759d6df8270e500e64e9097b3a7499e
Anekoique <ctolu01@gmail.com>
2025-12-01 15:57:20 +0800

    feat: add trait impls for mutable references

--- src/impls.rs
+++ src/impls.rs
@@ -1,10 +1,11 @@
+use alloc::{string::String, vec::Vec};
 use core::{cmp, mem};
 
 use axerrno::ax_bail;
 
 use crate::{
+    BufRead, Read, Result, Seek, SeekFrom, Write,
     buf::{Buf, BufMut},
-    Read, Result, Write,
 };
 
 impl Read for &[u8] {
@@ -103,5 +104,89 @@ impl BufMut for &mut [u8] {
         let filled = f(self)?;
         *self = mem::take(self).split_at_mut(filled).1;
         Ok(filled)
+    }
+}
+
+impl<R: Read + ?Sized> Read for &mut R {
+    #[inline]
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        (**self).read(buf)
+    }
+
+    #[inline]
+    fn read_exact(&mut self, buf: &mut [u8]) -> Result<()> {
+        (**self).read_exact(buf)
+    }
+
+    #[cfg(feature = "alloc")]
+    #[inline]
+    fn read_to_end(&mut self, buf: &mut alloc::vec::Vec<u8>) -> Result<usize> {
+        (**self).read_to_end(buf)
+    }
+
+    #[cfg(feature = "alloc")]
+    #[inline]
+    fn read_to_string(&mut self, buf: &mut alloc::string::String) -> Result<usize> {
+        (**self).read_to_string(buf)
+    }
+}
+
+impl<W: Write + ?Sized> Write for &mut W {
+    #[inline]
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        (**self).write(buf)
+    }
+
+    #[inline]
+    fn flush(&mut self) -> Result<()> {
+        (**self).flush()
+    }
+
+    #[inline]
+    fn write_all(&mut self, buf: &[u8]) -> Result<()> {
+        (**self).write_all(buf)
+    }
+
+    #[inline]
+    fn write_fmt(&mut self, fmt: core::fmt::Arguments<'_>) -> Result<()> {
+        (**self).write_fmt(fmt)
+    }
+}
+
+impl<S: Seek + ?Sized> Seek for &mut S {
+    fn seek(&mut self, pos: SeekFrom) -> Result<u64> {
+        (**self).seek(pos)
+    }
+
+    fn rewind(&mut self) -> Result<()> {
+        (**self).rewind()
+    }
+
+    fn stream_position(&mut self) -> Result<u64> {
+        (**self).stream_position()
+    }
+}
+
+impl<B: BufRead + ?Sized> BufRead for &mut B {
+    fn fill_buf(&mut self) -> Result<&[u8]> {
+        (**self).fill_buf()
+    }
+
+    fn consume(&mut self, amt: usize) {
+        (**self).consume(amt)
+    }
+
+    fn has_data_left(&mut self) -> Result<bool> {
+        (**self).has_data_left()
+    }
+
+    #[cfg(feature = "alloc")]
+    fn read_until(&mut self, byte: u8, buf: &mut Vec<u8>) -> Result<usize> {
+        (**self).read_until(byte, buf)
+    }
+
+    #[cfg(feature = "alloc")]
+    fn read_line(&mut self, buf: &mut String) -> Result<usize> {
+        (**self).read_line(buf)
     }
 }
